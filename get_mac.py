854f9d9cd15f224a61b12938784744f456ab9087
nayan8459 <naveen.nayan@zenatix.com>
2018-05-06 00:00:46 +0530

    interface fix 1

--- get_mac.py
+++ get_mac.py
@@ -21,9 +21,11 @@ class READMAC:
                 print(interface_name)
                 if "e" == interface_name[0]:
                     mac_eth = open(self.__interface + '/' + interface_name).readline()
+                    print(mac_eth)
                     data["eth"] = mac_eth.split("\n")[0]
                 elif "w" == interface_name[0]:
                     mac_wlan = open(self.__interface + '/' + interface_name).readline()
+                    print(mac_wlan)
                     data["wlan"] = mac_wlan.split("\n")[0]
                 else:
                     pass
