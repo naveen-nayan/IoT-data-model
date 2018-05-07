963cc49ff534e7b5610f6937f08198d7561b7919
nayan8459 <naveen.nayan@zenatix.com>
2018-05-07 20:53:55 +0530

    created data set in csv

--- fake_data.py
+++ fake_data.py
@@ -1,20 +1,28 @@
 
 import os
-import json
 import inspect
+import csv
+import random
 
+work_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# path for csv data set
+data_path = work_path + '/device_info.data'
+no_of_devices = input("Please Enter count of Device : ")
 
-for i in range(101, 200):
-    work_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    info_path = work_path + '/device_info.json'
-    cmd = "python {0}/device_info.py -d {1}".format(work_path, i)
-    os.system(cmd)
-    with open(info_path) as f:
-        json_data = json.load(f)
-    json_data['CPU_PER_DAY%'] = str(json_data["CPU%"])
-    json_data['MEMORY_PER_DAY%'] = str(json_data["MEMORY%"])
-    print json_data
-    with open(info_path, 'w') as outfile:
-        json.dump(json_data, outfile)
-    cmd = "python {0}/device_info.py".format(work_path)
-    os.system(cmd)
+# create a csv file as data set
+with open(data_path, 'w' ) as new_file:
+    fieldname = ['Device id', 'eth MAC', 'wlan MAC']
+    csv_writer = csv.DictWriter(new_file, fieldnames=fieldname, delimiter=',')
+    csv_writer.writeheader()
+    for i in range(1, no_of_devices+1):
+        data = {"Device id": "", "eth MAC": "", "wlan MAC": ""}
+        wlan = "{0:X}:{1:X}:{2:X}:{3:X}:{4:X}:{5:X}".format(random.randint(17, 255), random.randint(17, 255),
+                                                            random.randint(17, 255), random.randint(17, 255),
+                                                            random.randint(17, 255), random.randint(17, 255))
+        eth = "{0:X}:{1:X}:{2:X}:{3:X}:{4:X}:{5:X}".format(random.randint(17, 255), random.randint(17, 255),
+                                                            random.randint(17, 255), random.randint(17, 255),
+                                                            random.randint(17, 255), random.randint(17, 255))
+        data["Device id"] = i
+        data["eth MAC"] = eth
+        data["wlan MAC"] = wlan
+        csv_writer.writerow(data)
