d10f36c6d7ed06629cab9e3af1107c7feaab062a
nayan8459 <naveen.nayan@zenatix.com>
2018-05-07 22:34:54 +0530

    data integrity

--- fake_data.py
+++ fake_data.py
@@ -11,19 +11,20 @@ no_of_devices = input("Please Enter count of Device : ")
 
 # create a csv file as data set
 with open(data_path, 'w' ) as new_file:
-    fieldname = ['Device id', 'eth MAC', 'wlan MAC']
+    fieldname = ['Start bit', 'Device id', 'eth MAC', 'wlan MAC', 'Stop bit']
     csv_writer = csv.DictWriter(new_file, fieldnames=fieldname, delimiter=',')
-    csv_writer.writeheader()
     for i in range(1, no_of_devices+1):
-        data = {"Device id": "", "eth MAC": "", "wlan MAC": ""}
-        wlan = "{0:X}:{1:X}:{2:X}:{3:X}:{4:X}:{5:X}".format(random.randint(17, 255), random.randint(17, 255),
+        data = {"Start bit":"","Device id": "", "eth MAC": "", "wlan MAC": "", "Stop bit": ""}
+        wlan = "{0:b}:{1:b}:{2:b}:{3:b}:{4:b}:{5:b}".format(random.randint(17, 255), random.randint(17, 255),
                                                             random.randint(17, 255), random.randint(17, 255),
                                                             random.randint(17, 255), random.randint(17, 255))
-        eth = "{0:X}:{1:X}:{2:X}:{3:X}:{4:X}:{5:X}".format(random.randint(17, 255), random.randint(17, 255),
+        eth = "{0:b}:{1:b}:{2:b}:{3:b}:{4:b}:{5:b}".format(random.randint(17, 255), random.randint(17, 255),
                                                             random.randint(17, 255), random.randint(17, 255),
                                                             random.randint(17, 255), random.randint(17, 255))
+        data["Start bit"] = "00"
         data["Device id"] = i
         data["eth MAC"] = eth
         data["wlan MAC"] = wlan
+        data["Stop bit"] = "11"
         csv_writer.writerow(data)
 print ("data file present in {}".format(data_path))
