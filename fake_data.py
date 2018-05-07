adf16b8a53cf43bc47e6efd15bc29d9e074a0d57
nayan8459 <naveen.nayan@zenatix.com>
2018-05-07 10:47:23 +0530

    changed fake_data.sh to fake_data.py

--- fake_data.py
+++ fake_data.py
@@ -1,6 +1,18 @@
 
 import os
+import json
 import inspect
 
-work_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
+for i in range(101, 200):
+    work_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    info_path = work_path + '/device_info.json'
+    cmd = "python {0}/device_info.py -d {1}".format(work_path, i)
+    os.system(cmd)
+    with open(info_path) as f:
+        json_data = json.load(f)
+    json_data['CPU_PER_DAY%'] = str(json_data["CPU%"])
+    json_data['MEMORY_PER_DAY%'] = str(json_data["MEMORY%"])
+    print json_data
+    cmd = "python {0}/device_info.py".format(work_path)
+    os.system(cmd)
