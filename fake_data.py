5c5a2ef2fd3be19c701a4326ba47150e76b68b93
nayan8459 <naveen.nayan@zenatix.com>
2018-05-08 23:45:41 +0530

    second approach

--- fake_data.py
+++ fake_data.py
@@ -14,38 +14,31 @@ mac = "18:5e:0f:24:78:54"
 list size = 6
 '''
 
-start_address_wlan = 24  # start point for wlan mac
-start_address_eth = 80  # start point for eth mac
-list2 = range(0, 256)
-list3 = range(0, 256)
-list4 = range(0, 256)
-list5 = range(0, 256)
-list6 = range(0, 256)
-
 count = 0
+start_range = 0
+max_range = int("FFFFFFFFFFFF", 16)
+
+if no_of_devices > max_range:
+    exit()
 
 # create a csv file as data set
 with open(data_path, 'w') as new_file:
     fieldname = ['Device id', 'eth MAC', 'wlan MAC']
     csv_writer = csv.DictWriter(new_file, fieldnames=fieldname, delimiter=',')
-    # for i in range(1, no_of_devices + 1):
-    for item2 in list2:
-        for item3 in list3:
-            for item4 in list4:
-                for item5 in list5:
-                    for item6 in list6:
-                        mac_generated = "{0:02x}:{1:02x}:{2:02x}:{3:02x}:{4:02x}".format(item2, item3, item4, item5,
-                                                                                         item6)
-                        data = {"Device id": "", "eth MAC": "", "wlan MAC": ""}
-                        wlan = "{0:02x}:{1}".format(start_address_wlan, mac_generated)
-                        eth = "{0:02x}:{1}".format(start_address_eth, mac_generated)
-                        data["Device id"] = "{0}".format(count + 1)
-                        data["eth MAC"] = eth
-                        data["wlan MAC"] = wlan
-                        csv_writer.writerow(data)
-                        sys.stdout.write('\r' + "Creating data set for : " +str(count + 1))
-                        count += 1
-                        if count == no_of_devices:
-                            exit()
-                            pass
-print ("data file present in {}".format(data_path))
+    while True:
+        data = {"Device id": "", "eth MAC": "", "wlan MAC": ""}
+        mac_generated = "{0:016x}".format(start_range)
+        list1 = []
+        while mac_generated:
+            list1.append(mac_generated[:2])
+            mac_generated = mac_generated[2:]
+        mac = "{0}:{1}:{2}:{3}{4}:{5}".format(list1[0], list1[1], list1[2], list1[3], list1[4], list1[5])
+        data["Device id"] = "{0}".format(count + 1)
+        data["eth MAC"] = mac
+        data["wlan MAC"] = mac
+        csv_writer.writerow(data)
+        sys.stdout.write('\r' + "Creating data set for : " +str(count + 1))
+        count += 1
+        if count == no_of_devices:
+            exit()
+# print ("data file present in {}".format(data_path))
