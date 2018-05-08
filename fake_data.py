dab227fec4d709fd820a39822a4613b6d4582308
nayan8459 <naveen.nayan@zenatix.com>
2018-05-08 23:50:35 +0530

    fix 1

--- fake_data.py
+++ fake_data.py
@@ -14,7 +14,6 @@ mac = "18:5e:0f:24:78:54"
 list size = 6
 '''
 
-count = 0
 start_range = 0
 max_range = int("FFFFFFFFFFFF", 16)
 
@@ -32,13 +31,14 @@ with open(data_path, 'w') as new_file:
         while mac_generated:
             list1.append(mac_generated[:2])
             mac_generated = mac_generated[2:]
+        print list1
         mac = "{0}:{1}:{2}:{3}{4}:{5}".format(list1[0], list1[1], list1[2], list1[3], list1[4], list1[5])
-        data["Device id"] = "{0}".format(count + 1)
+        data["Device id"] = "{0}".format(start_range + 1)
         data["eth MAC"] = mac
         data["wlan MAC"] = mac
         csv_writer.writerow(data)
-        sys.stdout.write('\r' + "Creating data set for : " +str(count + 1))
-        count += 1
-        if count == no_of_devices:
+        sys.stdout.write('\r' + "Creating data set for : " +str(start_range + 1))
+        start_range += 1
+        if start_range == no_of_devices:
             exit()
 # print ("data file present in {}".format(data_path))
